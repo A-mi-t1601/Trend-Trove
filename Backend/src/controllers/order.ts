98a76aa9fb112146dd15c21ad08d86a2b8816184
A-mi-t1601 <amit.r.jhinkwan16@gmai.com>
2024-07-30 19:00:33 +0530

    Create Dashboard, Pie, Bar & Line chart API.

--- Backend/src/controllers/order.ts
+++ Backend/src/controllers/order.ts
@@ -80,7 +80,7 @@ export const newOrder = TryCatch(
       total,
     });
     await reduceStock(orderItems);
-    await invalidateCache({
+    invalidateCache({
       product: true,
       order: true,
       admin: true,
@@ -112,7 +112,7 @@ export const processOrder = TryCatch(async (req, res, next) => {
   }
 
   await order.save();
-  await invalidateCache({
+  invalidateCache({
     product: false,
     order: true,
     admin: true,
@@ -132,7 +132,7 @@ export const deleteOrder = TryCatch(async (req, res, next) => {
   if (!order) return next(new ErrorHandler("Order Not Found", 404));
 
   await order.deleteOne();
-  await invalidateCache({
+  invalidateCache({
     product: false,
     order: true,
     admin: true,
--- Backend/src/controllers/product.ts
+++ Backend/src/controllers/product.ts
@@ -97,7 +97,7 @@ export const NewProduct = TryCatch(
       category: category.toLowerCase(),
       photo: photo.path,
     });
-    await invalidateCache({ product: true });
+    invalidateCache({ product: true, admin: true });
 
     return res.status(201).json({
       success: true,
@@ -125,7 +125,11 @@ export const updateProduct = TryCatch(async (req, res, next) => {
   if (category) product.category = category;
 
   await product.save();
-  await invalidateCache({ product: true, productId: String(product._id) });
+  invalidateCache({
+    product: true,
+    productId: String(product._id),
+    admin: true,
+  });
 
   return res.status(200).json({
     success: true,
@@ -141,7 +145,11 @@ export const deleteProduct = TryCatch(async (req, res, next) => {
   });
 
   await Product.deleteOne();
-  await invalidateCache({ product: true, productId: String(product._id) });
+  invalidateCache({
+    product: true,
+    productId: String(product._id),
+    admin: true,
+  });
 
   return res.status(200).json({
     success: true,
